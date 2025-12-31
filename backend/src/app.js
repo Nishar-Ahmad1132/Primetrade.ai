3dbb4a626251e4630e30633ba216e12201800dd9
Nishar Ahmad <ahmadnishar1132@gmail.com>
2025-12-31 16:18:28 +0530

    setup for frontend deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
